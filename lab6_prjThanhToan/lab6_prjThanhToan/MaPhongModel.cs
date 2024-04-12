1b08841b0234f4e945ef9bcaa3ab5b3345c1acd2
Mee18 <nguyenseoul18@gmail.com>
2024-04-12 18:54:57 +0700

    add PhongModel.cs

---
+++ lab6_prjThanhToan/lab6_prjThanhToan/MaPhongModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class MaPhongModel
+{
+
+    public MaPhongModel() { }
+
+    public int _maPhong {  get; set; }
+    public string _tenPhong { get; set; }
+}
