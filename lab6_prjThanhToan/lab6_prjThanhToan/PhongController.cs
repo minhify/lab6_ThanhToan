ebffbbf4a089c5e0f2921446c4c45e74a9620455
Mee18 <nguyenseoul18@gmail.com>
2024-04-12 20:03:01 +0700

    add Phong controller

---
+++ lab6_prjThanhToan/lab6_prjThanhToan/PhongController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6_prjThanhToan
+{
+    class PhongController
+    {
+        List<PhongModel.Phong> GetPhong()
+        {
+            List<PhongModel.Phong> listPhong = new List<PhongModel.Phong>();
+            try
+            {
+                Database.OpenConnection();
+                SqlDataAdapter adapter = new SqlDataAdapter("Select MaPhong, TenPhong from Phong", Database.connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    PhongModel.Phong  phong = new PhongModel.Phong();
+                    phong._maPhong = Convert.ToInt32(row["MaPhong"]);
+                    phong._tenPhong = row["TenPhong"].ToString();
+                    listPhong.Add(phong);
+                }   
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error: " + ex.Message);
+            }
+            return listPhong;
+        }
+
+    }
+}
